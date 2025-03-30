b4177f9b521602e5c918d9c78624b4aa14b5307e
Koss86 <jacobdelarosa123@gmail.com>
2025-03-30 08:29:05 -0500

    Neovim config

---
+++ init.lua
@@ -0,0 +1,16 @@
+vim.opt.expandtab=true
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.opt.mouse = 'a'
+vim.opt.showmode = false 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.have_nerd_font = true
+
+require("config.lazy")
