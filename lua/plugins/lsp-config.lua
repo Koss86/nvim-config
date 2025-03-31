2e5b2c1e5f148f60d3c13f0055405b60d41b9012
Koss86 <jacobdelarosa123@gmail.com>
2025-03-30 21:30:20 -0500

    added icon settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,4 +1,12 @@
 return {
   "williamboman/mason.nvim",
-  reqire("mason").setup()
+  require("mason").setup({
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
+      }
+    }
+  })
 }
