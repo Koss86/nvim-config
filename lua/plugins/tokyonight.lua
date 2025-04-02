b586e22a92272fe45fac20f55f83441b158d6b39
Koss86 <jacobdelarosa123@gmail.com>
2025-04-02 09:27:08 -0500

    Working out how these damn plugins work

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,14 +2,16 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {
-    style = "night",
-    styles = {
-      comments = { italic = false },
-      keywords = { italic = true },
-    },
-    cache = true,
-  },
+  config = function ()
+    require("tokyonight").setup({
+      style = "day",
+      styles = {
+	comments = { italic = true }, -- is it true
+	keywords = { italic = true },
+      },
+      cache = true,
+    })
+  end, 
   on_colors = function(colors)
     colors.hint = colors.orange
     colors.error = "#ff0000"
