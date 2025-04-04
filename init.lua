bd4bcad7e41167f676824152e41362e4674a86ba
Koss86 <jacobdelarosa123@gmail.com>
2025-04-03 20:42:03 -0500

    Added which-key

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ vim.opt.scrolloff = 15
 vim.opt.inccommand = 'split'
 vim.opt.confirm = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -31,7 +31,9 @@ return {
 					enable_document_symbols = true,
 				},
 			})
-			lspconfig.zls.setup({})
+			lspconfig.zls.setup({
+				capabilities = capabilities,
+      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
