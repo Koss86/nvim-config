eaabaa27e28f3f1549d65443197d6215741330ea
Koss86 <jacobdelarosa123@gmail.com>
2025-04-11 01:31:59 -0500

    add keymaps for telescope fine_files, live_grep, and help_tags. And added .editorconfig for lang specific formatting.

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -1,34 +1,68 @@
 return {
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     lazy = false,
     opts = {
       ui = {
         icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
+          package_installed = '✓',
+          package_pending = '➜',
+          package_uninstalled = '✗'
         }
       }
     }
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    'williamboman/mason-lspconfig.nvim',
     lazy = false,
     opts = {
       ensure_installed = {},
     }
   },
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
+    dependencies = { {
+      'folke/lazydev.nvim',
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        },
+      },
+    },
+    },
     config = function()
-      local lspconfig = require("lspconfig")
+      local lspconfig = require('lspconfig')
+
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
       --lspconfig.ols.setup({})
       --lspconfig.zls.setup({})
       --lspconfig.gopls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {} )
-    end,
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.supports_method('textDocument/implementation') then
+            vim.keymap.set('n', '<leader>fi', function() vim.lsp.buf.implementation() end, {})
+          end
+          --          if client.supports_method('textDocument/completion') then
+          --           vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+          --         end
+          if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+      vim.keymap.set('n', '<leader>m', function() vim.lsp.buf.format() end, {})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    end
   }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,18 @@
 return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
+  config = function()
+    local builtin = require("telescope.builtin")
+    require("telescope").setup({
+      pickers = { find_files = { theme = "ivy" } },
+    })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+    vim.keymap.set("n", "<leader>ff", builtin.find_files)
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>en", function()
+      builtin.find_files({
+        cwd = vim.fn.stdpath("config")
+      })
+    end)
+  end
 }
