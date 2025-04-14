ad89f6ea6b56095a19d04a3780443ddf86805848
Koss86 <jacobdelarosa123@gmail.com>
2025-04-14 16:58:55 -0500

    still working out how lsps work

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -1,41 +1,41 @@
 return {
   {
-    'williamboman/mason.nvim',
+    "williamboman/mason.nvim",
     lazy = false,
     opts = {
       ui = {
         icons = {
-          package_installed = '✓',
-          package_pending = '➜',
-          package_uninstalled = '✗'
-        }
-      }
-    }
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
   },
   {
-    'williamboman/mason-lspconfig.nvim',
+    "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
       ensure_installed = {},
-    }
+    },
   },
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       {
-        'folke/lazydev.nvim',
+        "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
         opts = {
           library = {
             -- See the configuration section for more details
             -- Load luvit types when the `vim.uv` word is found
-            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
       },
     },
     config = function()
-      local lspconfig = require('lspconfig')
+      local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
@@ -43,20 +43,27 @@ return {
       --lspconfig.zls.setup({})
       --lspconfig.gopls.setup({})
 
-      --[[
-      vim.api.nvim_create_autocmd('LspAttach', {
+      vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client.supports_method('textDocument/implementation') then
-            vim.keymap.set('n', '<leader>fi', function() vim.lsp.buf.implementation() end, {})
+          ---@diagnostic disable-next-line: param-type-mismatch
+          if client.supports_method("textDocument/implementation") then
+            vim.keymap.set("n", "<leader>fi", function()
+              vim.lsp.buf.implementation()
+            end, {})
           end
+          --[[
+          if client.supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+          end
+  ]]
 
-          -- if client.supports_method('textDocument/completion') then
-          --   vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-          -- end
-
-          if client.supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
+          ---@diagnostic disable-next-line: param-type-mismatch
+          if client.supports_method("textDocument/formatting") then
+            vim.keymap.set("n", "<leader>m", function()
+              vim.lsp.buf.format()
+            end, {})
+            vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
@@ -65,18 +72,22 @@ return {
           end
         end,
       })
-]]
-      vim.keymap.set('n', '<leader>m', function() vim.lsp.buf.format() end, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
   },
   {
     "nvimtools/none-ls.nvim",
-    --[[
-config = function()
-  local null_ls = require("null-ls")
-end
-]]
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.diagnostics.eslint,
+          --null_ls.builtins.formatting.clangd,
+        },
+      })
+    end,
   },
 }
