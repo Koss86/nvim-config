4dc361d26338542e1e0d315512bde9eb89612424
Koss86 <jacobdelarosa123@gmail.com>
2025-04-14 17:27:14 -0500

    playin with stuff trying to figure it out

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -84,7 +84,6 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
           --null_ls.builtins.formatting.clangd,
         },
       })
