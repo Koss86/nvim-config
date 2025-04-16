1c883094e5fe41a8a4c65666fb1da9a6e4abf5a4
Koss86 <jacobdelarosa123@gmail.com>
2025-04-16 06:50:42 -0500

    Added gitsigns and ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,38 @@
 return {
-    {
+  {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-},
-{
+  },
+  {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-        require("telescope").setup({
-            extensions = {
-                ["ui-select"] = {
-                    -- themes: get_dropdown, get_cursor, get_ivy
-                    require("telescope.themes").get_dropdown {
-                        -- more opts
-                    }
-                }
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        },
+        extensions = {
+          ["ui-select"] = {
+            -- themes: get_dropdown, get_cursor, get_ivy
+            require("telescope.themes").get_cursor {
+              -- more opts
             }
+          }
+        }
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config")
         })
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files)
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-        vim.keymap.set("n", "<leader>en", function()
-            builtin.find_files({
-                cwd = vim.fn.stdpath("config")
-            })
-        end)
+      end)
+    end,
+    init = function()
+      require("telescope").load_extension("ui-select")
     end
-}
+  }
 }
