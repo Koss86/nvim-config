4f85258f175fb98c0dd367c219cb8523545a8877
Koss86 <jacobdelarosa123@gmail.com>
2025-04-16 06:49:49 -0500

    Setup lua_ls and found out out to turn text for diagnostics on"

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,17 +1,79 @@
+---@diagnostic disable: missing-parameter, need-check-nil, param-type-mismatch
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                },
-            },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
         },
+      },
     },
-    {
-        
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = {},
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+    config = function()
+      require("lspconfig").lua_ls.setup({
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        capabilities = capabilities,
+        settings = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.supports_method("textDocument/implementation") then
+            vim.keymap.set("n", "<leader>fi", function() vim.lsp.buf.implementation() end, {})
+          end
+
+          if client.supports_method("textDocument/formatting") then
+            vim.keymap.set("n", "<leader>m", function() vim.lsp.buf.format() end)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  },
+  {
+
+  }
 }
