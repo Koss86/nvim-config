dcae8936780fac467ad5700c15b8660b1a3c021e
Koss86 <jacobdelarosa123@gmail.com>
2025-04-17 08:35:29 -0500

    misc

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -7,6 +7,27 @@ return {
   config = function()
     local dap, dapui = require("dap"), require("dapui")
 
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "codelldb",
+      name = "lldb",
+    }
+
+    dap.configurations.c = {
+      {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = { "-Wall", "-g" },
+        runInTerminal = false,
+      },
+    }
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -20,7 +41,7 @@ return {
       dapui.close()
     end
 
-    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>tb", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
     vim.keymap.set("n", "<leader>dc", dap.continue, {})
   end,
 }
