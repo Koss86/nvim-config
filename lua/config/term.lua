dfdf96f6b86d0ddfb031bd9fbff7ae43c717feb9
Koss86 <jacobdelarosa123@gmail.com>
2025-04-23 17:16:01 -0500

    misc

--- lua/config/term.lua
+++ lua/config/term.lua
@@ -10,12 +10,12 @@ function M.toggle()
       vim.api.nvim_win_close(term_winid, true)
       term_winid = nil
     else
-      vim.cmd("5split")
+      vim.cmd("7split")
       term_winid = vim.api.nvim_get_current_win()
       vim.api.nvim_win_set_buf(term_winid, term_bufnr)
     end
   else
-    vim.cmd("5split")
+    vim.cmd("7split")
     term_winid = vim.api.nvim_get_current_win()
     vim.cmd("terminal")
     term_bufnr = vim.api.nvim_get_current_buf()
