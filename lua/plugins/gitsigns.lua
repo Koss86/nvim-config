e68c488870c6400d73bede59c7751b02fe939cc4
Koss86 <jacobdelarosa123@gmail.com>
2025-04-24 07:36:20 -0500

    misc

--- lua/plugins/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    local gitsigns = require("gitsigns")
-    gitsigns.setup({})
-    vim.keymap.set("n", "<leader>dt", gitsigns.diffthis, { desc = "[d]iff [t]his File" })
-  end,
-}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,3 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  lazy = false,
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    window = {
-      width = 27,
-      height = 15,
-    },
-  },
-  vim.keymap.set("n", "<leader>f", "<cmd>Neotree toggle<cr>"),
+
 }
