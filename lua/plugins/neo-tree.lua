cf97804f66ccb5a5e768849307cbd9ea6aacd29b
Koss86 <jacobdelarosa123@gmail.com>
2025-04-24 17:01:02 -0500

    wip

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,19 @@
 return {
-
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    window = {
+      position = "left",
+      width = 26,
+    },
+  },
+  vim.keymap.set("n", "<leader>f", "<cmd>Neotree toggle<cr>")
 }
