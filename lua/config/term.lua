bfe085bceb21b62df7ade3b6c2ba0ef0a68c9160
Koss86 <jacobdelarosa123@gmail.com>
2025-04-25 12:46:17 -0500

    Misc

--- lua/config/term.lua
+++ lua/config/term.lua
@@ -1,24 +1,24 @@
 local M = {}
 
 -- Internal state
-local term_bufnr = nil
+local term_bufnum = nil
 local term_winid = nil
 
 function M.toggle()
-  if term_bufnr and vim.api.nvim_buf_is_valid(term_bufnr) then
+  if term_bufnum and vim.api.nvim_buf_is_valid(term_bufnum) then
     if term_winid and vim.api.nvim_win_is_valid(term_winid) then
       vim.api.nvim_win_close(term_winid, true)
       term_winid = nil
     else
       vim.cmd("7split")
       term_winid = vim.api.nvim_get_current_win()
-      vim.api.nvim_win_set_buf(term_winid, term_bufnr)
+      vim.api.nvim_win_set_buf(term_winid, term_bufnum)
     end
   else
     vim.cmd("7split")
     term_winid = vim.api.nvim_get_current_win()
     vim.cmd("terminal")
-    term_bufnr = vim.api.nvim_get_current_buf()
+    term_bufnum = vim.api.nvim_get_current_buf()
   end
 end
 
