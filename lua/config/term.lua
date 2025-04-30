a477e666479e71d9ecd805d16cf0cebd912673ba
Koss86 <jacobdelarosa123@gmail.com>
2025-04-29 20:34:14 -0500

    .

--- lua/config/term.lua
+++ lua/config/term.lua
@@ -1,5 +1,23 @@
 M = {}
-local win_num = nil
-local buf_num = nil
+local win_id = nil
+local buf_id = nil
+
+function M.toggle()
+  if buf_id and vim.api.nvim_buf_is_valid(buf_id) then
+    if win_id and vim.api.nvim_win_is_valid(win_id) then
+      vim.api.nvim_win_close(win_id, true)
+      win_id = nil
+  else
+    vim.cmd("7split")
+    win_id = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win_id, buf_id)
+    end
+  else
+    vim.cmd("7split")
+    win_id = vim.api.nvim_get_current_win()
+    vim.cmd("terminal")
+    buf_id = vim.api.nvim_get_current_buf()
+  end
+end
 
 return M
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight"
+      },
+      sections = {
+        lualine_c = { "filename", "lsp_status" }
+      }
+    })
+  end
+}
