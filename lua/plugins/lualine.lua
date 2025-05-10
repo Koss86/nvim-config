89b7d602d05c4c1073bd2513a0246489a32e3ccc
Koss86 <jacobdelarosa123@gmail.com>
2025-05-10 05:34:49 -0500

    Changed position of LSP info

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,9 +8,9 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
+      lualine_c = { "filename", "lsp_status" },
       lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress", "lsp_status" },
+      lualine_y = { "progress" },
       lualine_z = { "location" },
     },
   },
