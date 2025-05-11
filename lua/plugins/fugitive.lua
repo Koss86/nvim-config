200204935a735c61b5ea31b06b22d8363bdd77f6
Koss86 <jacobdelarosa123@gmail.com>
2025-05-11 09:18:03 -0500

    Added fugitive.

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,6 @@
+return {
+  "tpope/vim-fugitive",
+  vim.keymap.set("n", "<leader>fd", "<cmd>Git diff<cr>"),
+  vim.keymap.set("n", "<leader>fl", "<cmd>Git log<cr>"),
+  vim.keymap.set("n", "<leader>fc", "<cmd>Git commit<cr>"),
+}
