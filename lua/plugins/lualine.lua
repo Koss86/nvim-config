71d99eec0b5e8564c82c0c158e2fe8ac9c9985de
Koss86 <jacobdelarosa123@gmail.com>
2025-06-06 00:19:52 -0500

    Lualine: rearranged lsp_status and filename

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename", "lsp_status" },
+      lualine_c = { "lsp_status", "filename" },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
