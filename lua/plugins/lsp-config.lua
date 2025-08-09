0b80cfdd42b2646ab01f7aed5145e362242aae99
Koss86 <jacobdelarosa123@gmail.com>
2025-08-09 13:02:54 -0500

    Added a more complete config options for clangd

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -51,7 +51,51 @@ return {
       local lsp = vim.lsp
 
       lsp.config("clangd", {
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        cmd = { "clangd" },
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+        root_markers = {
+          ".clangd",
+          ".clang-tidy",
+          ".clang-format",
+          "compile_commands.json",
+          "compile_flags.txt",
+          "configure.ac", -- AutoTools
+          ".git",
+        },
+        capabilities = {
+          textDocument = {
+            completion = {
+              editsNearCursor = true,
+            },
+          },
+          offsetEncoding = { "utf-8", "utf-16" },
+        },
+        ---@param client vim.lsp.Client
+        ---@param init_result ClangdInitializeResult
+        on_init = function(client, init_result)
+          if init_result.offsetEncoding then
+            client.offset_encoding = init_result.offsetEncoding
+          end
+        end,
+        on_attach = function(_, bufnr)
+          vim.api.nvim_buf_create_user_command(
+            bufnr,
+            "LspClangdSwitchSourceHeader",
+            function()
+              switch_source_header(bufnr)
+            end,
+            { desc = "Switch between source/header" }
+          )
+
+          vim.api.nvim_buf_create_user_command(
+            bufnr,
+            "LspClangdShowSymbolInfo",
+            function()
+              symbol_info()
+            end,
+            { desc = "Show symbol info" }
+          )
+        end,
       })
 
       lsp.config("lua_ls", {
