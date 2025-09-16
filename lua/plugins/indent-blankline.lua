3fb0419adad46d986660e29f85e26cc8ca0e8fdf
Jacob Delarosa <jacobdelarosa123@gmail.com>
2025-09-15 23:28:45 -0500

    Added tabchar to indent-blankline.lua
    Only way I could get this to work with go for some reason.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,12 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  lazy = true,
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  ---@module "ibl"
-  ---@type ibl.config
-  opts = {},
+    "lukas-reineke/indent-blankline.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+        debounce = 100,
+        indent = { tab_char = "|" },
+    },
 }
