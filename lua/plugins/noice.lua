29b2f353cc9cb445e8beed74e2fdf075c14ac637
Jacob Delarosa <jacobdelarosa123@gmail.com>
2025-10-02 09:46:22 -0500

    Disabled notify in noice.nvim
    Disabled to read long error messages using :messages
    While enabled, they wouldn't appear in :messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,6 +7,9 @@ return {
 
   config = function()
     require("noice").setup({
+      notify = {
+        enabled = false,
+      },
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
