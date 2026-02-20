0d788802d1d0ec99f703774b84044404fee6f2a2
Jacob Delarosa <jacobdelarosa123@gmail.com>
2026-02-20 05:22:49 -0600

    Updates to custom.lua's functions.
    Reworked some of the toggle terminal code. Most noticeably is that
    'clear' is no longer called in .compile(). Instead a string of '\b' is
    used to clear the terminal in case there are any stray key presses in
    the prompt. This way, the prompt history is preserved until a manual
    'clear'.

--- lua/config/custom.lua
+++ lua/config/custom.lua
@@ -1,33 +1,54 @@
 local M = {
   term_bufnum = nil,
   term_winid = nil,
+  clr_term = nil,
 }
-
--- Checks if the terminal is still running
+-- Check if the terminal is still running
 local function is_term_active(bufNum)
   if not bufNum or not vim.api.nvim_buf_is_valid(bufNum) then
     return false
   end
-  local ok, job_id = pcall(vim.api.nvim_buf_get_var, bufNum, "terminal_job_id")
-  if not ok or not job_id then
-    return false
-  end
+  local job_id = vim.bo[bufNum].channel
   return vim.fn.jobwait({ job_id }, 0)[1] == -1
 end
 
+local function open_split_term()
+  if not (is_term_active(M.term_bufnum)) then
+    -- If no terminal is active.
+    vim.cmd("9split | terminal")
+    M.term_winid = vim.api.nvim_get_current_win()
+    M.term_bufnum = vim.api.nvim_get_current_buf()
+    M.clr_term = false
+    return
+  elseif not (M.term_winid and vim.api.nvim_win_is_valid(M.term_winid)) then
+    -- If a terminal is active but not open.
+    vim.cmd("9split")
+    M.term_winid = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(M.term_winid, M.term_bufnum)
+  else
+    -- If terminal is active & open.
+    vim.api.nvim_set_current_win(M.term_winid)
+  end
+
+  M.clr_term = true
+end
+
 -- Toggle terminal in a split window.
-function M.toggleSplitTerm()
-  if M.term_bufnum and vim.api.nvim_buf_is_valid(M.term_bufnum) then
+function M.toggle_split_term()
+  if is_term_active(M.term_bufnum) then
     if M.term_winid and vim.api.nvim_win_is_valid(M.term_winid) then
+      -- If active and open.
       vim.api.nvim_win_close(M.term_winid, true)
       M.term_winid = nil
       return
     else
+      -- If active but not open.
       vim.cmd("9split")
       M.term_winid = vim.api.nvim_get_current_win()
       vim.api.nvim_win_set_buf(M.term_winid, M.term_bufnum)
     end
   else
+    -- If no terminal is active or open.
     vim.cmd("9split | terminal")
     M.term_winid = vim.api.nvim_get_current_win()
     M.term_bufnum = vim.api.nvim_get_current_buf()
@@ -45,6 +66,7 @@ function M.compile()
   local bin
   local cmd
 
+  -- Setup 'cmd' based off filetype.
   if ft == "c" then
     bin = vim.fn.expand("%:t")
     bin = string.sub(bin, 1, -3)
@@ -63,32 +85,15 @@ function M.compile()
     return
   end
 
-  if not (is_term_active(M.term_bufnum)) then
-    vim.cmd("9split | terminal")
-    M.term_winid = vim.api.nvim_get_current_win()
-    M.term_bufnum = vim.api.nvim_get_current_buf()
-  elseif not (M.term_winid and vim.api.nvim_win_is_valid(M.term_winid)) then
-    vim.cmd("9split")
-    M.term_winid = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(M.term_winid, M.term_bufnum)
-  else
-    vim.api.nvim_set_current_win(M.term_winid)
+  open_split_term()
+
+  local job_id = vim.bo[M.term_bufnum].channel
+
+  if M.clr_term then
+    vim.fn.chansend(job_id, "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b")
   end
 
-  local ok, job_id =
-    pcall(vim.api.nvim_buf_get_var, M.term_bufnum, "terminal_job_id")
-
-  if ok and job_id then
-    vim.fn.chansend(job_id, "\nclear\n")
-    vim.fn.chansend(job_id, cmd .. "\n")
-  else
-    vim.cmd("9split | terminal")
-    M.term_winid = vim.api.nvim_get_current_win()
-    M.term_bufnum = vim.api.nvim_get_current_buf()
-    job_id = vim.b.terminal_job_id
-    vim.fn.chansend(job_id, "\nclear\n")
-    vim.fn.chansend(job_id, cmd .. "\n")
-  end
+  vim.fn.chansend(job_id, cmd .. "\n")
 
   if ft == "c" then
     vim.fn.chansend(job_id, "./" .. bin .. "\n")
@@ -103,32 +108,15 @@ end
 function M.make_project()
   local cmd = "make install prefix=$HOME/.local"
 
-  if not (is_term_active(M.term_bufnum)) then
-    vim.cmd("9split | terminal")
-    M.term_winid = vim.api.nvim_get_current_win()
-    M.term_bufnum = vim.api.nvim_get_current_buf()
-  elseif not (M.term_winid and vim.api.nvim_win_is_valid(M.term_winid)) then
-    vim.cmd("9split")
-    M.term_winid = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(M.term_winid, M.term_bufnum)
-  else
-    vim.api.nvim_set_current_win(M.term_winid)
+  open_split_term()
+
+  local job_id = vim.bo[M.term_bufnum].channel
+
+  if M.clr_term then
+    vim.fn.chansend(job_id, "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b")
   end
 
-  local ok, job_id =
-    pcall(vim.api.nvim_buf_get_var, M.term_bufnum, "terminal_job_id")
-
-  if ok and job_id then
-    vim.fn.chansend(job_id, "\nclear\n")
-    vim.fn.chansend(job_id, cmd .. "\n")
-  else
-    vim.cmd("9split | terminal")
-    M.term_winid = vim.api.nvim_get_current_win()
-    M.term_bufnum = vim.api.nvim_get_current_buf()
-    local new_job_id = vim.b.terminal_job_id
-    vim.fn.chansend(new_job_id, "\nclear\n")
-    vim.fn.chansend(new_job_id, cmd .. "\n")
-  end
+  vim.fn.chansend(job_id, cmd .. "\n")
 
   vim.defer_fn(function()
     vim.fn.feedkeys("i")
@@ -136,7 +124,7 @@ function M.make_project()
 end
 
 -- Open lazygit. Tap q twice to exit.
-function M.openLazyGit()
+function M.open_lazy_git()
   vim.cmd("enew | terminal lazygit")
   vim.defer_fn(function()
     vim.fn.feedkeys("i")
