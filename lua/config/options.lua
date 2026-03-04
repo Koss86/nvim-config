a56a84ba14c317a204b816cdfcbaa59a425eccb6
Jacob Delarosa <jacobdelarosa123@gmail.com>
2026-03-04 03:20:35 -0600

    Typo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,7 +36,7 @@ vim.schedule(function()
   vim.o.clipboard = "unnamedplus"
 end)
 
--- Hightlight when yanking, taken from kickstart
+-- Highlight when yanking, taken from kickstart
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup(
