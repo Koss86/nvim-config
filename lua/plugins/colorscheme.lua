4be2ec79006f2a8eeb8e700bf44afa1adfa5c134
Jacob Delarosa <jacobdelarosa123@gmail.com>
2026-03-12 07:19:57 -0500

    Removed Kanagawa & Catppuccin colorschemes; added Rose Pine.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,120 +3,53 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     lazy = true,
-    opts = {
-      transparent = true,
-      style = "night",
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = true, bold = true },
-        functions = { italic = false, bold = true },
-        variables = { italic = false, bold = false },
-        dim_inactive = true,
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      on_colors = function(colors)
-        colors.hint = colors.orange
-        colors.error = "#ff0000"
-      end,
-    },
-    init = function()
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        style = "night",
+        styles = {
+          comments = { italic = true, bold = true },
+          keywords = { italic = false, bold = true },
+          functions = { italic = false, bold = true },
+          variables = { italic = false, bold = false },
+          dim_inactive = true,
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        on_colors = function(colors)
+          colors.hint = colors.orange
+          colors.error = "#ff0000"
+        end,
+      })
       vim.cmd("colorscheme tokyonight")
     end,
   },
-
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
+    "rose-pine/neovim",
     priority = 1000,
+    -- lazy = true,
+    name = "rose-pine",
 
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = {     -- :h background
-          light = "latte",
-          dark = "mocha",
+      require("rose-pine").setup({
+        variant = "moon",      -- auto, main, moon, or dawn
+        dark_variant = "main", -- main, moon, or dawn
+        dim_inactive_windows = true,
+        extend_background_behind_borders = true,
+
+        enable = {
+          terminal = true,
+          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+          migrations = true,        -- Handle deprecated options automatically
         },
-        transparent_background = true, -- disables setting the background color.
-        float = {
-          transparent = false,         -- enable transparent floating windows
-          solid = false,               -- use solid styling for floating windows, see |winborder|
-        },
-        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false,             -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = true,              -- Force no italic
-        no_bold = false,               -- Force no bold
-        no_underline = false,          -- Force no underline
-        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" },     -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = { "bold" },
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-          -- miscs = {}, -- Uncomment to turn off hard-coded styles
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = true,
         },
       })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("kanagawa").setup({
-        compile = false,  -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = false },
-        functionStyle = {},
-        keywordStyle = { italic = false, bold = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true,    -- do not set background color
-        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {             -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        theme = "wave",  -- Load "wave" theme
-        background = {   -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-
-      -- setup must be called before loading
-      vim.cmd("colorscheme kanagawa")
+      vim.cmd("colorscheme rose-pine")
     end,
   },
 }
