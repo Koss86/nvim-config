85d64a5419f9020e27eaa9149eb93ef3a7c17de5
Jacob Delarosa <jacobdelarosa123@gmail.com>
2026-03-12 07:03:40 -0500

    Add mason-lspconfig.nvim to auto install configured lsp's.
    Added hourglass icon to package_pending in Mason.

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,19 +1,25 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    event = "VeryLazy",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+  "mason-org/mason-lspconfig.nvim",
+  event = "VeryLazy",
+  opts = {
+    ensure_installed = { "bashls", "lua_ls", "marksman", "ols", "typos_lsp" },
+    automatic_enable = false,
+  },
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "",
+            package_uninstalled = "✗",
+          },
         },
       },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    {
+      "neovim/nvim-lspconfig",
+    },
   },
 }
